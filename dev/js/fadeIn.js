26138d231bed0ce35e6f86e08326f3e9978aee07
yuchinma <63366430+yuchinma@users.noreply.github.com>
2021-06-03 21:24:25 -0400

    reset folder structures

--- dev/js/fadeIn.js
+++
@@ -1,5 +0,0 @@
-import { gsap } from "gsap";
-
-export let fadeInTL = gsap.timeline();
-fadeInTL.from("#background-fill",{alpha:0, duration:3, scale:20})
-        .from("#clouds g",{alpha:0, stagger:0.25, duration:0.5},"-=2");
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,26 +1,19 @@
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 
-import { zoomTL } from "./zoomIn"
-import { fadeInTL } from "./fadeIn"
-import { spaceShipTL } from "./space-ship"
-import { liftOffTL } from "./listOff"
-import { flightTL } from "./flightPath"
-
+// import { sunsetTL } from "./sunset"
 
 gsap.registerPlugin(GSDevTools);
 
-let mainTL = gsap.timeline({paused:true});
+let mainTL = gsap.timeline();
 
-mainTL.add(fadeInTL)
-        .add(zoomTL)
-        .add(spaceShipTL)
-        .add(liftOffTL)
-        .addLabel("marker")
-        .add(flightTL);
+// mainTL.add(sunsetTL)
+        // .add(wavesTL, "-=2");
 
-        mainTL.play("marker");
+        // .addLabel("marker")
 
+        // mainTL.play("marker");
+        mainTL.play();
 
 // console.log(numberThing);
 
