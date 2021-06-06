1dffa18d0468d0b04f6845a4b2e65d57c0fda93e
yuchinma <63366430+yuchinma@users.noreply.github.com>
2021-06-06 14:01:36 -0400

    init logo animation done

--- dev/js/initLogoAnim.js
+++ dev/js/initLogoAnim.js
@@ -2,17 +2,25 @@ import { gsap } from "gsap";
 
 export let initLogoAnimTL = gsap.timeline();
 
-gsap.set("#orange-mtn",{transformOrigin:"center"});
-gsap.set("#red-mtn",{transformOrigin:"center"});
-gsap.set(".front-mtns",{transformOrigin:"center"});
-gsap.set(".trees",{transformOrigin:"center"});
+gsap.set("#trident-top, #headlight-left, #headlight-right, #whole-maserati-logo", {transformOrigin: "center"});
+gsap.set("#left-light-set-block", {x: "-=45"});
 
+initLogoAnimTL.to("#trident-strokes-block-left", {duration: .3, x: "-=50", ease: "none"}, "stroke")
+        .to("#trident-strokes-block-right", {duration: .3, x: "+=50", ease: "none"}, "stroke")
+        .from("#trident-base", {duration: .3, y: "+50"})
+        .fromTo("#trident-top", {y: "-=100", alpha: 0}, {duration: .5, y: "+=100", alpha: 1, ease: "back"})
 
-initLogoAnimTL.from("#orange-mtn",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
-            .from("#red-mtn",{duration:5.75, scale:10,ease: "power4.out",y:"+=800"},"zoom")
-            .from(".front-mtns",{duration:5.5, scale:10,ease: "power4.out",y:"+=800"},"zoom")
-            .from("#trees-5",{duration:5.5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-4",{duration:5.25, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-3",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-2",{duration:5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
-            .from("#trees-1",{duration:4.75, scale:10,ease: "power4.out",y:"+=420"},"zoom")
+        .from("#left-light-stroke-1, #left-light-stroke-2, #left-light-stroke-3, #left-light-stroke-4, #headlight-left", {alpha: 0}, "light")
+        .from("#right-light-stroke-1, #right-light-stroke-2, #right-light-stroke-3, #right-light-stroke-4, #headlight-right", {alpha: 0}, "light")
+        .to("#left-light-set-block", {duration: 1, x: "-=350", ease: "none"}, "light")
+        .to("#right-light-set-block", {duration: 1, x: "+=350", ease: "none"}, "light")
+
+        .to("#levante-logo-anim", {alpha: 0})
+        .to("#maserati-block-left", {duration: .5, x: "-=200"}, "maserati")
+        .to("#maserati-block-right", {duration: .5, x: "+=200"}, "maserati")
+        .to("#maserati-logo-anim", {alpha: 0})
+        .to("#levante-logo-anim", {alpha: 1}, "-=.3")
+        .to("#levante-block-left", {duration: .5, x: "-=150"}, "levante", "-=.7")
+        .to("#levante-block-right", {duration: .5, x: "+=150"}, "levante", "-=.7")
+
+        .fromTo("#whole-maserati-logo", {scale: 1}, {duration: 1.5, x: "+=400", y: "-=1000", alpha: 0, scale: 20, ease: "power4.out"}, "+=.2")
